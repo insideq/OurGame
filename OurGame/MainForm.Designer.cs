4c04937670dacb46418a98deb4258136267d6d87
insideq <crazyandroed@gmail.com>
2025-03-28 18:18:57 +0400

    С кайфом

---
+++ OurGame/MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace OurGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button8 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = Properties.Resources.door;
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.Location = new Point(62, 37);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 233);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = Properties.Resources.door;
+            button2.BackgroundImageLayout = ImageLayout.Zoom;
+            button2.Location = new Point(295, 37);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 233);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.BackgroundImage = Properties.Resources.door;
+            button3.BackgroundImageLayout = ImageLayout.Zoom;
+            button3.Location = new Point(520, 37);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 233);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.BackgroundImage = Properties.Resources.door;
+            button4.BackgroundImageLayout = ImageLayout.Zoom;
+            button4.Location = new Point(741, 37);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 233);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.BackgroundImage = Properties.Resources.door;
+            button5.BackgroundImageLayout = ImageLayout.Zoom;
+            button5.Location = new Point(62, 352);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 233);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.BackgroundImage = Properties.Resources.door;
+            button6.BackgroundImageLayout = ImageLayout.Zoom;
+            button6.Location = new Point(295, 352);
+            button6.Name = "button6";
+            button6.Size = new Size(112, 233);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.BackgroundImage = Properties.Resources.door;
+            button7.BackgroundImageLayout = ImageLayout.Zoom;
+            button7.Location = new Point(520, 352);
+            button7.Name = "button7";
+            button7.Size = new Size(112, 233);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.BackgroundImage = Properties.Resources.door;
+            button9.BackgroundImageLayout = ImageLayout.Zoom;
+            button9.Location = new Point(741, 352);
+            button9.Name = "button9";
+            button9.Size = new Size(112, 233);
+            button9.TabIndex = 8;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.BackgroundImage = Properties.Resources.door;
+            button10.BackgroundImageLayout = ImageLayout.Zoom;
+            button10.Location = new Point(960, 352);
+            button10.Name = "button10";
+            button10.Size = new Size(112, 233);
+            button10.TabIndex = 9;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.BackgroundImage = Properties.Resources.персонаж;
+            button8.BackgroundImageLayout = ImageLayout.Zoom;
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(932, 69);
+            button8.Margin = new Padding(0);
+            button8.Name = "button8";
+            button8.Size = new Size(165, 168);
+            button8.TabIndex = 10;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.ковер;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(1137, 643);
+            Controls.Add(button8);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "MainForm";
+            Text = "Всем привет с вами демон и андроид";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button9;
+        private Button button10;
+        private Button button8;
+    }
+}
